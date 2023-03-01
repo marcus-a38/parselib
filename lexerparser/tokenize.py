72d95fe8cd10a8eb82a14fc3871556a1c87309cf
Marcus Antonelli <marcus.an38@gmail.com>
2023-03-01 01:28:21 -0500

    Cleaned up code, scrapped some ideas, debugged and functional.

--- lexerparser/tokenize.py
+++ lexerparser/tokenize.py
@@ -1,69 +1,50 @@
 import re
 
-def verify_token(method):
+class Lexer():
 
-    def inner(token):
-        if token in '' or token in ' ':
-            raise TypeError('Token cannot be empty.')
-        else:
-            return method(token)
-    return inner()
 
+    def __init__(self, custom_breaks=None):
+
+        self.custom_breaks = custom_breaks
+
+
+    def tokenize(self, str, tags_enabled=False):
+
+        empty_entries = [' ', '']
+        tokens = re.split('(\W)', str)
+        tokens[:] = [token for token in tokens if token not in empty_entries]
+    
+        return [Token(token) for token in tokens]
+
+
+    def defragment(self, token_list, bounds=(0,0)):
+
+        if len(bounds) > 2: 
+            raise ValueError('Range must only have a left and right bound.')
+      
+        left = bounds[0]
+        right = bounds[1] - 1
+        temp = ''
+
+        try:
+            for i in range(left, right):    
+                temp += token_list[i].value+' '
+            return temp
+
+        except IndexError as e: 
+            exit(f'Concatenation halted because {e}. Largest index is {i} and you inputted {right} as a right bound.')
+      
 
 class Token():
 
     index = 0
-    tags = ['variable', 'keyword', 'delimiter', 'operator', 'value', 'comment']
+    tag_types = ['variable', 'keyword', 'delimiter', 'operator', 'value', 'comment']
+  
+    def __init__(self, curr):
 
-    @verify_token
-    def __init__(self, token, tag='None', role='None'):
-
-        if tag not in 'None': self.tag = tag
-        if role not in 'None': self.role = role
-        self.value = token
+        self.tag = 'None'
+        self.role = 'None'
+        self.expression_index = 'None'
+        self.line = 'None'
+        self.value = curr
         self.index = Token.index
-        Token.index += 1
-
-
-    def create_tags():
-        pass
-
-
-    def tokenize(str, tags_enabled=False):
-
-        empty_entries = [' ', '']
-
-        tokens = re.split('(\W)', tokens)
-        tokens[:] = [token for token in tokens if token not in empty_entries]
-
-        if tags_enabled:
-            pass
-        else:
-            pass
-
-        return tokens
-
-
-class Queue():
-
-    def __init__(self, tokens):
-        self.queue = tokens
-
-
-    def push_queue(self, target):
-        for i in len(self.queue):
-            target(self.queue.pop(i))
-
-# UNFINISHED!
-
-                
-            
-
-
-
-
-
-
-
-
-
